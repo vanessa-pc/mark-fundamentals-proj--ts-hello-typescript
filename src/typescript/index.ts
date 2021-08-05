20c64016afb1a29c25daade051614407bea1c5da
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-05 15:53:47 +0300

    Fix three errors in index.ts file

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,17 +1,17 @@
 let numberOne = 5;
 const numberTwo = 10;
-numberOne = "7";
-
+numberOne = 7;
 const additionResult = numberOne + numberTwo;
 console.log(`${numberOne} plus ${numberTwo} is... ${additionResult}`);
 
 const arrOne = [1, 2, 3];
 const arrTwo = [4, 5, 6];
-const joinedArr = arrOne + arrTwo;
+const joinedArr = arrOne.concat(arrTwo);
+// const joinedArray = [...arrOne, ...arrTwo] --more modern syntax
 console.log(`Array addition: ${arrOne} + ${arrTwo} is... ${joinedArr}`);
 
 const rectangle = { width: 4, height: 10 };
-const area = rectangle.width * rectangle.heihgt; // an easy typo to make!
+const area = rectangle.width * rectangle.height; // an easy typo to make!
 console.log(
   `Rectangle area: ${rectangle.width} x ${rectangle.height} is... ${area}`
 );
